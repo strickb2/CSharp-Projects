0ff23ff00b3664f7515594bf6ec5bc988b7bc54b
strickb2 <minzckx6>
2020-12-29 21:01:18 +0000

    C# Programs

---
+++ AssignGrade/AssignGrade/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AssignGrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int grade;
+            bool studentPassed;
+            Console.Write("Enter Grade: ");
+            grade = Convert.ToInt32(Console.ReadLine());
+            studentPassed = grade >= 55 ? true : false; 
+            Console.WriteLine("Student has passed: {0}", studentPassed);
+            Console.Read();
+        }
+    }
+}
---
+++ AssignMultiply/AssignMultiply/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AssignMultiply
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3, sum;
+            Console.Write("Enter the First Number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Second Number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Third Number: ");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            sum = num1 * num2 * num3;
+            Console.WriteLine("The sum is: {0} ", sum);
+            Console.Read();
+        }
+    }
+}
