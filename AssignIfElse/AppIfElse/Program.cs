4370eef3af0718111811d4ae9199c85798d9c7bb
strickb2 <minzckx6>
2020-12-30 21:21:29 +0000

    C# Programs

---
+++ AssignIfElse/AppIfElse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AssignIfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            Console.WriteLine("Enter first number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter third number: ");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            if(num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine("{0} is the largest number", num1);
+            }
+            else if(num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine("{0} is the largest number", num2);
+            }
+            else
+            {
+                Console.WriteLine("{0} is the largest number", num3);
+            }
+        }
+    }
+}
