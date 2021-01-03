1d920e92cb1ccfb543bfdb84ce907649ce7190d4
strickb2 <minzckx6>
2021-01-03 18:24:10 +0000

    C# Programs

---
+++ AssignArray/AssignArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AssignArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] elements = new int[10];
+            int i, temp;
+            for (i = 0; i < elements.Length - 1; i++)
+            {
+                Console.WriteLine("Enter number: ");
+                elements[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (i = 0; i < elements.Length - 1; i++)
+            {
+                for (int j = i + 1; j < elements.Length; j++)
+                {
+                    if (elements[i] < elements[j])
+                    {
+                        temp = elements[i];
+                        elements[i] = elements[j];
+                        elements[j] = temp;
+                    }
+                }
+            }
+            foreach(int element in elements)
+                {
+                    Console.WriteLine(" {0}", element);
+                }                              
+        }
+    }
+}
---
+++ AssignFactorial/AssignFactorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AssignFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, sum= 1, i;
+            Console.WriteLine("Enter number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (i=1; i <= num; i++)
+            {
+                sum *= i;
+            }
+            Console.WriteLine("The factorial of {0} is {1}", num, sum);
+        }
+    }
+}
