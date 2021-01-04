a66645e468f7c3c7d514de8aa4bb9bfce86046a6
strickb2 <minzckx6>
2021-01-04 13:49:51 +0000

    C# Programs

---
+++ AssignInterest/AssignInterest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AssignInterest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name;
+            int LoanTime;
+            double InterestRate, LoanAmount, SimpleInterest;
+            try
+            {
+                Console.WriteLine("Enter Name: ");
+                Name = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Enter Loan Amount: ");
+                LoanAmount = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter Loan Period (in years): ");
+                LoanTime = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Interest Rate: ");
+                InterestRate = Convert.ToDouble(Console.ReadLine());
+                SimpleInterest = LoanAmount * LoanTime * InterestRate;
+                Console.WriteLine("{0}: Simple Interest Rate is {1}", Name, SimpleInterest);
+            }
+            catch(FormatException ex1)
+            {
+                Console.WriteLine("Invalid input format...{0}", ex1.Message);
+            }
+            
+        }
+    }
+}
